254c02ff5a613b7232d48b67c72d43dd97b3aefb
Robbie Dowie <robbiedowie@yahoo.com>
2025-07-24 14:12:49 +0100

    Subset lists exercise

---
+++ SubsetLists.py
@@ -0,0 +1,15 @@
+# List of household and age 
+
+household = [["Robbie", 27],
+             ["Sarah", 27],
+             ["Koda", 5],
+             ["Pablo", 4]]
+
+#print sarah details 
+
+print(household[1][:])
+
+
+# print pablo age
+
+print(household[3][1])
