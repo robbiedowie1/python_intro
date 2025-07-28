f20654c626ffcbc29d5216338532ca5f84c6e372
Robbie Dowie <robbiedowie@yahoo.com>
2025-07-28 13:09:02 +0100

    new

---
+++ AddToList.py
@@ -0,0 +1,11 @@
+household = ["Robbie", 27, "Sarah", 27, "Koda", 5, "Pablo", 4]
+
+household[0] = "Rob"
+
+#add a new pet fish to household - new varible 
+
+household_fish = household + ["Peter the fish", 1]
+
+print(household_fish)
+
+
---
+++ RemoveFromList.py
@@ -0,0 +1,7 @@
+household = ['Rob', 27, 'Sarah', 27, 'Koda', 5, 'Pablo', 4, 'Peter the fish', 1]
+
+#Fish has died - remove him 
+
+del household[-2:]
+
+print(household)
