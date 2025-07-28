8100975b2823915d685679c3a338d7adf38ad2c2
Robbie Dowie <robbiedowie@yahoo.com>
2025-07-28 13:43:38 +0100

    all basic functions

---
+++ Functions.py
@@ -0,0 +1,33 @@
+family = ["robbie", "sarah", "koda", "pablo"]
+ages = [27, 27, 5, 4]
+
+#Print type 
+print(type(ages))
+print(type(family))
+
+
+# max (biggest) age
+
+max_age = max(ages)
+print(max_age)
+print(max(ages))
+
+#find smallest age
+youngest = min(ages)
+print(youngest)
+print(min(ages))
+
+#round ages
+round(27.11)
+
+#everyones ages 
+
+total_ages = sum(ages)
+print(total_ages)
+print(sum(ages))
+
+#print length
+print(len(ages))
+
+#print sorted 
+print(sorted(ages))
